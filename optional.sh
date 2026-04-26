2a327bffa76b216e136c61ecf36d93c4b1d228fe
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-04-26 14:05:53 +0700

    yt

--- optional.sh
+++ optional.sh
@@ -14,4 +14,6 @@ grep -A 10 Port "$YTSCRIPT"
 
 chmod +x "$YTSCRIPT"
 
+sed -i "/# Triger-Add/a\    alias yt-download='$HOME/File/Script/random/yt-download.sh'" ~/.config/fish/config.fish
+
 pkg install ffmpeg
